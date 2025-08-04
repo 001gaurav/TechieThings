31a3041475be808cae9a1747b8e099416aa5be3b
001gaurav <ahiregaurav3122@gmail.com>
2025-08-05 00:13:22 +0530

    We create Dbcontext class for domain models TechPost and Category etc.

---
+++ API/TechieThings.API/TechieThings.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TechieThings.API.Models.Domain;
+
+namespace TechieThings.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<TechPost> TechPosts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
